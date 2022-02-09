c5706f6d3139f399ab70e3fa0d96744cf2af48fd
Sergio <sergiquijano@gmail.com>
2022-02-09 17:11:00 +0100

    feat: added function to normalize frequencies of words

--- bag_of_words.jl
+++ bag_of_words.jl
@@ -29,10 +29,44 @@ function load_dataset() ::BagOfWords
     words = Vector{Word}()
 
     # Iterate over our dataset
+    curr_it = 0
     for row in eachrow(df)
         curr_word = Word(row["palabra"], row[" frecuencia normalizada"])
         push!(words, curr_word)
+
+        # TODO -- delete this. I used this to speed up computations
+        curr_it += 1
+        if curr_it == 20
+            break
+        end
     end
 
     return BagOfWords(words)
+end
+
+"""
+Normalizes the frequencies of the words in the bag, to represent a probability distribution (that's
+to say, sum of probs add up to 1)
+"""
+function normalize_frequencies(bag::BagOfWords) ::BagOfWords
+    total_words = big(0)
+
+    # Compute the total sum of frequencies
+    for word in bag.words
+        total_words += word.frequency
+    end
+
+    # Create new bag of words using normalized frecuencies
+    # Normalize using prev computed value
+    new_words = Vector{Word}()
+    for word in bag.words
+        new_word = Word(word.content, word.frequency / total_words)
+        push!(new_words, new_word)
+    end
+
+    return BagOfWords(new_words)
+end
+
+function length(bag::BagOfWords)
+    return length(bag.words)
 end
